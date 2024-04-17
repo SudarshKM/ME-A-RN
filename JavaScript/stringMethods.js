c464f7a34723c5f57b599a92d749e89ad20d5b5c
SudarshKM <sudarshkm85@gmail.com>
2024-04-17 11:00:39 +0530

    17th april

---
+++ JavaScript/stringMethods.js
@@ -0,0 +1,18 @@
+//w.a.p to check whether the given number is mobile no or not 
+
+ num = "1234567890";
+
+ console.log(num.length==10?"yes":"no");
+
+ //w.a.p to check whether the given mail id is gmail or not 
+
+ mail = "abc@gmail.com";
+
+   console.log(mail.endsWith("gmail.com")?"gmail":"other" );
+
+
+//w.a.p to check whether the string ends with q
+
+ str3="qabcq";
+
+  console.log(str3.startsWith('q')?"q":"not q");
